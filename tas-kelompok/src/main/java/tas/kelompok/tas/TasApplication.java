9c585b48edf94790eef54815053ab222541d750e
Ivansukma <ivanrzr12@gmail.com>
2020-11-25 20:45:30 +0700

    Update TasApplication.java

--- tas-kelompok/src/main/java/tas/kelompok/tas/TasApplication.java
+++ tas-kelompok/src/main/java/tas/kelompok/tas/TasApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class TtsApplication {
+public class TasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TtsApplication.class, args);
+		SpringApplication.run(TasApplication.class, args);
                 System.out.println("Program is running");
 	}
         
