97309b56c7b4e5d2e4d24b46b0d10bfc3318ecbf
Ivansukma <ivanrzr12@gmail.com>
2020-12-01 12:56:57 +0700

    Update cek_suhu.sql

--- cek_suhu.sql
+++ cek_suhu.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 30, 2020 at 02:13 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 01, 2020 at 05:56 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,12 +27,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `data_temperatur`
 --
 
-CREATE TABLE `data_temperatur` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `data_temperatur`;
+CREATE TABLE IF NOT EXISTS `data_temperatur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_form` int(11) NOT NULL,
   `tanggal_periksa` date NOT NULL,
   `suhu_tubuh` double NOT NULL,
-  `konfirmasi_suhu` tinyint(1) NOT NULL
+  `konfirmasi_suhu` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_form` (`id_form`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -41,15 +44,19 @@ CREATE TABLE `data_temperatur` (
 -- Table structure for table `form_kepentingan`
 --
 
-CREATE TABLE `form_kepentingan` (
-  `id_form` int(11) NOT NULL,
+DROP TABLE IF EXISTS `form_kepentingan`;
+CREATE TABLE IF NOT EXISTS `form_kepentingan` (
+  `id_form` int(11) NOT NULL AUTO_INCREMENT,
   `id_mahasiswa` varchar(10) DEFAULT NULL,
   `id_admin` varchar(10) DEFAULT NULL,
   `kepentingan` text NOT NULL,
   `riwayat_penyakit` varchar(200) NOT NULL,
   `fakultas_kunjungan` varchar(50) NOT NULL,
-  `status` enum('menunggu','diterima','ditolak') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `status` enum('menunggu','diterima','ditolak') DEFAULT NULL,
+  PRIMARY KEY (`id_form`),
+  KEY `id_admin` (`id_admin`),
+  KEY `id_mahasiswa` (`id_mahasiswa`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `form_kepentingan`
@@ -57,7 +64,14 @@ CREATE TABLE `form_kepentingan` (
 
 INSERT INTO `form_kepentingan` (`id_form`, `id_mahasiswa`, `id_admin`, `kepentingan`, `riwayat_penyakit`, `fakultas_kunjungan`, `status`) VALUES
 (2, 'USER-00034', NULL, 'Kunjungan Pendaftaran', 'Sehat Kok', 'FTI', NULL),
-(3, NULL, NULL, 'Cek Kampus', 'Sehat KOK', 'FTI', NULL);
+(3, NULL, NULL, 'Cek Kampus', 'Sehat KOK', 'FTI', NULL),
+(6, NULL, NULL, 'Ke kampus', 'Tidak ada', 'FTI', NULL),
+(7, NULL, NULL, 'Ke kampus', 'Tidak ada', 'FBS', NULL),
+(8, NULL, NULL, 'Ke kampus', 'Tidak ada', 'FTI', NULL),
+(9, NULL, NULL, 'Ke kampus', 'Tidak ada', 'FTI', NULL),
+(10, NULL, NULL, 'Ke kampus', 'Tidak ada', 'FTI', NULL),
+(11, NULL, NULL, 'Ke kampus', 'Tidak ada', 'FTI', NULL),
+(12, NULL, NULL, 'Ke kampus', 'Tidak ada', 'FTI', NULL);
 
 -- --------------------------------------------------------
 
@@ -65,14 +79,27 @@ INSERT INTO `form_kepentingan` (`id_form`, `id_mahasiswa`, `id_admin`, `kepentin
 -- Table structure for table `kuesioner`
 --
 
-CREATE TABLE `kuesioner` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `kuesioner`;
+CREATE TABLE IF NOT EXISTS `kuesioner` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_form` int(11) NOT NULL,
-  `gejala_umum` varchar(200) NOT NULL,
+  `gejala_umum` varchar(200) DEFAULT NULL,
   `kunjungan_RS` varchar(50) NOT NULL,
   `kunjungan_luarkota` varchar(50) NOT NULL,
-  `status_pernahdirawat_covid19` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `status_pernahdirawat_covid19` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_form` (`id_form`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kuesioner`
+--
+
+INSERT INTO `kuesioner` (`id`, `id_form`, `gejala_umum`, `kunjungan_RS`, `kunjungan_luarkota`, `status_pernahdirawat_covid19`) VALUES
+(1, 8, 'Batuk', 'Ya', 'Ya', 'Ya'),
+(2, 9, 'Batuk,Radang', 'Ya', 'Ya', 'Ya'),
+(3, 8, 'Batuk', 'Ya', 'Ya', 'Ya'),
+(4, 8, NULL, 'Ya', 'Ya', 'Ya');
 
 -- --------------------------------------------------------
 
@@ -80,9 +107,11 @@ CREATE TABLE `kuesioner` (
 -- Table structure for table `macam_gejala`
 --
 
-CREATE TABLE `macam_gejala` (
-  `id` int(11) NOT NULL,
-  `ragam_gejala` varchar(50) NOT NULL
+DROP TABLE IF EXISTS `macam_gejala`;
+CREATE TABLE IF NOT EXISTS `macam_gejala` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ragam_gejala` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -91,13 +120,15 @@ CREATE TABLE `macam_gejala` (
 -- Table structure for table `pengguna`
 --
 
-CREATE TABLE `pengguna` (
+DROP TABLE IF EXISTS `pengguna`;
+CREATE TABLE IF NOT EXISTS `pengguna` (
   `ID_Pengguna` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `telefon` varchar(20) NOT NULL,
   `status_daftarulang` tinyint(1) DEFAULT NULL,
-  `tanggal_boleh_daftarulang` date DEFAULT NULL
+  `tanggal_boleh_daftarulang` date DEFAULT NULL,
+  PRIMARY KEY (`ID_Pengguna`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -113,84 +144,14 @@ INSERT INTO `pengguna` (`ID_Pengguna`, `email`, `nama`, `telefon`, `status_dafta
 -- Table structure for table `persetujuan_user`
 --
 
-CREATE TABLE `persetujuan_user` (
+DROP TABLE IF EXISTS `persetujuan_user`;
+CREATE TABLE IF NOT EXISTS `persetujuan_user` (
   `tanggal_isiform` date NOT NULL,
   `id_kuesioner` int(11) DEFAULT NULL,
-  `id_gejala` int(11) DEFAULT NULL
+  `id_gejala` int(11) DEFAULT NULL,
+  KEY `id_kuesioner` (`id_kuesioner`),
+  KEY `id_gejala` (`id_gejala`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `data_temperatur`
---
-ALTER TABLE `data_temperatur`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_form` (`id_form`);
-
---
--- Indexes for table `form_kepentingan`
---
-ALTER TABLE `form_kepentingan`
-  ADD PRIMARY KEY (`id_form`),
-  ADD KEY `id_admin` (`id_admin`),
-  ADD KEY `id_mahasiswa` (`id_mahasiswa`);
-
---
--- Indexes for table `kuesioner`
---
-ALTER TABLE `kuesioner`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_form` (`id_form`);
-
---
--- Indexes for table `macam_gejala`
---
-ALTER TABLE `macam_gejala`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `pengguna`
---
-ALTER TABLE `pengguna`
-  ADD PRIMARY KEY (`ID_Pengguna`);
-
---
--- Indexes for table `persetujuan_user`
---
-ALTER TABLE `persetujuan_user`
-  ADD KEY `id_kuesioner` (`id_kuesioner`),
-  ADD KEY `id_gejala` (`id_gejala`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `data_temperatur`
---
-ALTER TABLE `data_temperatur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `form_kepentingan`
---
-ALTER TABLE `form_kepentingan`
-  MODIFY `id_form` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `kuesioner`
---
-ALTER TABLE `kuesioner`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `macam_gejala`
---
-ALTER TABLE `macam_gejala`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
