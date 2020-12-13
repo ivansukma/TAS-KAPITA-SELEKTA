f630803458164d82f72cd7838bbafc57d6f9264d
Ivansukma <ivanrzr12@gmail.com>
2020-12-13 15:53:27 +0700

    Update cek_suhu.sql

--- cek_suhu.sql
+++ cek_suhu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 11, 2020 at 01:33 PM
+-- Generation Time: Dec 13, 2020 at 08:52 AM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -36,14 +36,17 @@ CREATE TABLE IF NOT EXISTS `data_temperatur` (
   `konfirmasi_suhu` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_form` (`id_form`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `data_temperatur`
 --
 
 INSERT INTO `data_temperatur` (`id`, `id_form`, `tanggal_periksa`, `suhu_tubuh`, `konfirmasi_suhu`) VALUES
-(1, 8, '2020-12-08', 35, 0);
+(1, 8, '2020-12-08', 35, 0),
+(2, 8, '2020-12-11', 36, 1),
+(3, 17, '2020-12-11', 35, 1),
+(4, 18, '2020-12-26', 23, 1);
 
 -- --------------------------------------------------------
 
@@ -63,17 +66,20 @@ CREATE TABLE IF NOT EXISTS `form_kepentingan` (
   PRIMARY KEY (`id_form`),
   KEY `id_admin` (`id_admin`),
   KEY `id_mahasiswa` (`id_mahasiswa`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `form_kepentingan`
 --
 
 INSERT INTO `form_kepentingan` (`id_form`, `id_mahasiswa`, `id_admin`, `kepentingan`, `riwayat_penyakit`, `fakultas_kunjungan`, `status`) VALUES
-(2, 'USER-00034', NULL, 'Kunjungan Pendaftaran', 'Sehat Kok', 'FTI', 'menunggu'),
-(8, 'USER-00034', NULL, 'Ke kampus', 'Tidak ada', 'FTI', 'menunggu'),
+(2, 'USER-00034', NULL, 'Kunjungan Pendaftaran', 'Sehat Kok', 'FTI', 'diterima'),
+(8, 'USER-00034', NULL, 'Ke kampus', 'Tidak ada', 'FTI', 'diterima'),
 (9, 'USER-00034', NULL, 'Ke kampus', 'Tidak ada', 'FTI', 'ditolak'),
-(13, 'USER-00015', NULL, 'Tes', 'Gk Papa', 'FTI', 'diterima');
+(13, 'USER-00015', NULL, 'Tes', 'Gk Papa', 'FTI', 'diterima'),
+(17, 'USER-00017', NULL, 'Ke kampus', 'Tidak ada', 'FTI', 'ditolak'),
+(18, 'USER-00017', NULL, 'Ke kampus', 'Tidak ada', 'FTI', 'diterima'),
+(26, 'USER-00017', 'USER-00016', 'Ke kampus', 'Tidak ada', 'FTI', 'diterima');
 
 -- --------------------------------------------------------
 
@@ -91,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `kuesioner` (
   `status_pernahdirawat_covid19` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_form` (`id_form`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `kuesioner`
@@ -99,7 +105,9 @@ CREATE TABLE IF NOT EXISTS `kuesioner` (
 
 INSERT INTO `kuesioner` (`id`, `id_form`, `gejala_umum`, `kunjungan_RS`, `kunjungan_luarkota`, `status_pernahdirawat_covid19`) VALUES
 (1, 8, 'Batuk', 'Ya', 'Ya', 'Ya'),
-(2, 9, 'Batuk,Radang', 'Ya', 'Ya', 'Ya');
+(2, 9, 'Batuk,Radang', 'Ya', 'Ya', 'Ya'),
+(5, 8, 'Batuk', 'Ya', 'Ya', 'Ya'),
+(6, 26, 'Batuk', 'Ya', 'Ya', 'Ya');
 
 -- --------------------------------------------------------
 
@@ -125,8 +133,6 @@ CREATE TABLE IF NOT EXISTS `pengguna` (
   `ID_Pengguna` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
   `nama` varchar(50) NOT NULL,
-  `status_daftarulang` tinyint(1) DEFAULT NULL,
-  `tanggal_boleh_daftarulang` date DEFAULT NULL,
   PRIMARY KEY (`ID_Pengguna`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -134,11 +140,11 @@ CREATE TABLE IF NOT EXISTS `pengguna` (
 -- Dumping data for table `pengguna`
 --
 
-INSERT INTO `pengguna` (`ID_Pengguna`, `email`, `nama`, `status_daftarulang`, `tanggal_boleh_daftarulang`) VALUES
-('USER-00015', 'hartonomatthew0@gmail.com', 'Hartono', NULL, NULL),
-('USER-00016', '672018048@student.uksw.edu', 'Ivan Sukma Hanindria', NULL, NULL),
-('USER-00017', 'robertusbagaskara@gmail.com', 'Robertus Bagaskara Radite Putra', NULL, NULL),
-('USER-00034', 'matthewadian@yahoo.com', 'Matthew Adian', NULL, NULL);
+INSERT INTO `pengguna` (`ID_Pengguna`, `email`, `nama`) VALUES
+('USER-00015', 'hartonomatthew0@gmail.com', 'Matthew Adian Hartono'),
+('USER-00016', '672018048@student.uksw.edu', 'Ivan Sukma Hanindria'),
+('USER-00017', 'robertusbagaskara@gmail.com', 'Robertus Bagaskara Radite Putra'),
+('USER-00034', 'matthewadian@yahoo.com', 'Matthew Adian');
 
 --
 -- Constraints for dumped tables
